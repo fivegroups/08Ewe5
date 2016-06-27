f1401f0ed3cf54c6d91b97f63ddba158bea5a491
mttstudy <1412486710@qq.com>
2016-06-27 11:00:15 +0800

    add2

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=we7',
+    'username' => 'root',
+    'password' => 'admin',
+    'charset' => 'utf8',
+];
---
+++ web/pdo.php
@@ -0,0 +1,3 @@
+<?php
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=we7','root','admin');
+$pdo -> exec('set names utf8');
